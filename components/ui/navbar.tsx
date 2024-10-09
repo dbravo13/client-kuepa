62a6f04382abcda80017e8249f6a361fdf9cf28c
David Bravo <david.bravo13@hotmail.com>
2024-10-08 22:08:01 -0500

    Final

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -5,13 +5,19 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 import { signOut } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 function Navbar() {
   const router = useRouter();
+  const { data: session, status } = useSession();
+
+  // Verifica si el usuario tiene el rol de 'admin'
+  const isAdmin = session?.user?.role === "admin";
 
   const handleGo = () => {
     router.push("/class-room/users");
   };
+
   return (
     <nav className="flex items-center justify-between px-4 py-3 bg-white border-b border-gray-200">
       <div className="mb-2">
@@ -25,9 +31,16 @@ function Navbar() {
       </div>
 
       <div className="flex items-center space-x-2">
-        <Button variant="ghost" className="text-primero-100" onClick={handleGo}>
-          Usuarios
-        </Button>
+        {isAdmin && (
+          <Button
+            variant="ghost"
+            className="text-primero-100"
+            onClick={handleGo}
+          >
+            Usuarios
+          </Button>
+        )}
+
         <Button className="bg-primero-100" onClick={() => signOut()}>
           Salir
         </Button>
